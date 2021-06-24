0c4892c4b7c8661c29c3193c359191bdb29347b5
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-06-24 11:40:34 +0200

    fler testfall för addition och subtraktion

--- bordsräknare.test/UnitTest1.cs
+++ bordsräknare.test/UnitTest1.cs
@@ -9,30 +9,31 @@ namespace bordsräknare.test
 	[Theory]
 	[InlineData(  2, (new double[]{ 1, 1}))]
 	[InlineData(  1, (new double[]{ 1}))]
-	[InlineData( 21, (new double[]{ 20,1]))]
+	[InlineData( 21, (new double[]{ 20,1}))]
 	[InlineData(717, (new double[]{ 234, 435, 43, 5, -1, 1}))]
 	[InlineData( 14, (new double[]{ -12, -3, 5, 24}))]
 	public void TestaAdd( double förväntat, double[]termer)
 	{
 	    Program prog = new Program();
 	    double summa=prog.Addition( termer);
-	    Console.WriteLine( summa.ToString(), förväntat.ToString());
+	    Console.WriteLine( "{0} {1}", summa.ToString(), förväntat.ToString());
 	    Assert.Equal( förväntat, summa);
 	}
     }
     public class Subtraktion
     {
 	[Theory]
-	[InlineData(  0, (new double[]{ 1, 1}))]
-	[InlineData(  1, (new double[]{ 1}))]
-	[InlineData( 19, (new double[]{ 20,1]))]
-	[InlineData(249, (new double[]{ 234, 435, 43, 5, -1, 1}))]
-	[InlineData(-38, (new double[]{ -12, -3, 5, 24}))]
+	[InlineData(   0, (new double[]{ 1, 1}))]
+	[InlineData(   1, (new double[]{ 1}))]
+	[InlineData(  -3, (new double[]{ -3}))]
+	[InlineData(  19, (new double[]{ 20,1}))]
+	[InlineData(-249, (new double[]{ 234, 435, 43, 5, -1, 1}))]
+	[InlineData( -38, (new double[]{ -12, -3, 5, 24}))]
 	public void TestaSubtraktion(double förväntat, double[] termer)
 	{
 	    Program prog = new Program();
 	    double resultat=prog.Subtraktion(termer);
-	    Console.WriteLine( resultat.ToString(), förväntat.ToString());
+	    Console.WriteLine( "{0} {1}", resultat.ToString(), förväntat.ToString());
 	    Assert.Equal( förväntat, resultat);
 	}
     }
