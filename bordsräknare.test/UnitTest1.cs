6a8b635975c6164d5e14a0639c685c604429e753
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-06-28 10:56:16 +0200

    test för division
    låt den ensamma testrunnern i varje klass ha samma namn som i de övriga

--- bordsräknare.test/UnitTest1.cs
+++ bordsräknare.test/UnitTest1.cs
@@ -14,7 +14,7 @@ namespace bordsräknare.test
 	[InlineData( 21, (new double[]{ 20,1}))]
 	[InlineData(717, (new double[]{ 234, 435, 43, 5, -1, 1}))]
 	[InlineData( 14, (new double[]{ -12, -3, 5, 24}))]
-	public void TestaAdd( double förväntat, double[]termer)
+	public void RunTest( double förväntat, double[]termer)
 	{
 	    Program prog = new Program();
 	    double summa=prog.Addition( termer);
@@ -31,7 +31,7 @@ namespace bordsräknare.test
 	[InlineData(  19, (new double[]{ 20,1}))]
 	[InlineData(-249, (new double[]{ 234, 435, 43, 5, -1, 1}))]
 	[InlineData( -38, (new double[]{ -12, -3, 5, 24}))]
-	public void TestaSubtraktion(double förväntat, double[] termer)
+	public void RunTest(double förväntat, double[] termer)
 	{
 	    Program prog = new Program();
 	    double resultat=prog.Subtraktion(termer);
@@ -54,6 +54,21 @@ namespace bordsräknare.test
 	    Program prog = new Program();
 	    double resultat=prog.Multiplikation(faktorer);
 	    Console.WriteLine( "{0} {1}", resultat.ToString(), förväntat.ToString());
+
+    public class Division
+    {
+	[Theory]
+	[InlineData(   1,      (new double[]{ 1, 1}))]
+	[InlineData(   1,      (new double[]{ 1}))]
+	[InlineData(  -3,      (new double[]{ -3}))]
+	[InlineData(  20,      (new double[]{ 20,1}))]
+	[InlineData(-21884850, (new double[]{ 234, 435, 43, 5, -1, 1}))]
+	[InlineData( 4320,     (new double[]{ -12, -3, 5, 24}))]
+	public void RunTest(double förväntat, double[] faktorer)
+	{
+	    Program prog = new Program();
+	    double resultat = prog.Multiplikation( faktorer);
+	    // Console.WriteLine( "mult {0} {1}", resultat.ToString(), förväntat.ToString());
 	    Assert.Equal( förväntat, resultat);
 	}
     }
